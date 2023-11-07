2299a2b13cbbaa80d9a37cfe389b2fcec33517ba
Kroched <Nekitonich4@yandex.ru>
2023-11-07 11:35:57 +0300

    add template

---
+++ lib/day_offs/my_source/my_source.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DayOffs::MySource
+  VERSION = "0.1.0"
+end
---
+++ lib/day_offs/sources/source_template.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module DayOffs::Sources
+  class SourceTemplate < Base
+    with_countries :RU # add countries supported by your source
+    with_years 2023, 2024 # add years supported by your source
+    with_name :source_template # add your source name
+
+    def call
+      [
+        DayOffs::DayOff.new(country, Date.civil(year))
+      ] # return array of DayOffs::DayOff structs
+    end
+  end
+end
